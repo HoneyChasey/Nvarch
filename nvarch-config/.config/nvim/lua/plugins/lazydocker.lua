ac9e504bd5856678523f86945a7c624940626842
honey <44633479+HoneyChasey@users.noreply.github.com>
2026-05-29 08:45:26 +0200

    update: better lazydocker with lazy

--- nvarch-config/.config/nvim/lua/plugins/lazydocker.lua
+++ nvarch-config/.config/nvim/lua/plugins/lazydocker.lua
@@ -1,13 +1,4 @@
 return {
   "HoneyChasey/lazydocker.nvim",
   opts = {},
-  keys = {
-    {
-    "<leader>ld", -- or change with your mapping key
-      function()
-        require("lazydocker").open()
-      end,
-      desc = "Open Lazydocker",
-    },
-  },
 }
