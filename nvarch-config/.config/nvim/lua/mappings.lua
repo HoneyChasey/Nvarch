df5cdb8002a95e210eaf4f84d792c3a2069fff9b
arch master <arch.master@gmail.com>
2026-07-01 22:32:56 +0200

    fix: cleaning code. Feature: adding docker compose file type to the config and adding docker ls and docker file lsps

--- nvarch-config/.config/nvim/lua/mappings.lua
+++ nvarch-config/.config/nvim/lua/mappings.lua
@@ -5,7 +5,6 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
 
 -- Lazygit mapping 
 map("n", "<leader>g", "<cmd>lua require('lazygit').lazygit()<CR>", { desc = "Open LazyGit" })
