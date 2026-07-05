ad504ad25db42ec964df7d0ddbb4e3cf2bbf74ab
arch master <arch.master@gmail.com>
2026-07-06 00:25:26 +0200

    fix: dashboard missing default config

--- nvarch-config/.config/nvim/lua/configs/dashboard-nvim.lua
+++ nvarch-config/.config/nvim/lua/configs/dashboard-nvim.lua
@@ -1,10 +1,37 @@
-require("dashboard").setup(){
-  theme = "doom",
+require("dashboard").setup({
+  theme = "hyper",
   config = {
     header = {
-      "Welcole back",
+      "",
+      "  ███╗   ██╗██╗   ██╗██╗███╗   ███╗",
+      "  ████╗  ██║██║   ██║██║████╗ ████║",
+      "  ██╔██╗ ██║██║   ██║██║██╔████╔██║",
+      "  ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+      "  ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║",
+      "  ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝",
+      "",
+    },
+    shortcut = {
+      { desc = "  Find File", group = "Label", action = "Telescope find_files", key = "f" },
+      { desc = "  Quit", group = "DiagnosticError", action = "qa", key = "q" },
+    },
+    project = {
+      enable = true,
+      limit = 8,
+      icon = "  ",
+      label = "Recent Projects",
+      action = "Telescope find_files cwd=",
+    },
+    mru = {
+      enable = true,
+      limit = 10,
+      icon = "  ",
+      label = "Recent Files",
+      cwd_only = false,
+    },
+    footer = {
+      "",
+      " Powered by lua "
     }
-  }
-
-}
-
+  },
+})
---
+++ todo.md
@@ -0,0 +1,9 @@
+# Issues 
+ - [ ] The lua status bar, keep changing possitions if i have horizontal termial open. The bar doesn't stop moving. 
+ - [ ] Put the navbar maybe on the top idk
+ - [ ] Cannot open at the same time a vertical termial and horizontal
+ - [x] Cannot change hte dashboard configration
+ - [ ] NvimTree bar is heere and the status bar don't get the size of the screen.
+ - [ ] Custom the status-bar
+ - [ ] Finish to custom the dashboard 
+ - [ ] When i close a file with ctrl x or :q, this kill all the window and i don't keep, the space for other open files. Like the cursor going directly on nvim-tree
