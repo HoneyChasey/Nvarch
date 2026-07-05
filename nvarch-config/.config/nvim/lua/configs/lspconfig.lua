5dec0974a9618f278eade9402d4f166726e09526
arch master <arch.master@gmail.com>
2026-07-05 23:50:22 +0200

    hotfix: display the correctly error now

--- nvarch-config/.config/nvim/lua/configs/lspconfig.lua
+++ nvarch-config/.config/nvim/lua/configs/lspconfig.lua
@@ -1,4 +1,6 @@
-vim.diagnostic.config({ virtual_text = false, virtual_lines = { current_line = false},})  -- display lsp diagnostic in line for the user. Change current_line to true to display only the diagnostic when cursor is on the specific line. Only work natively with neovim >= 0.11
+vim.diagnostic.config({
+  virtual_text = true,
+  virtual_lines = false}) -- display lsp diagnostic in line for the user. Change current_line to true to display only the diagnostic when cursor is on the specific line. Only work natively with neovim >= 0.11
 local servers = { "html", "cssls", "bashls", "clangd", "dockerls", "docker_compose_language_service", "gopls", "jdtls", "lua_ls", "pyright", "svelte", "nil_ls", "rust_analyzer"} -- Write here all the the name of lsp you want to enable. To get their name, please refer to https://github.com/neovim/nvim-lspconfig--
 
 --Exemple i want to add enable svelte to my nvim go to the repo and search the name of the language.--
