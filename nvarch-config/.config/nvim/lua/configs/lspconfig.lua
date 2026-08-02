82a622a21d6b15aa603c3eb126fa4c5402047560
HoneyChasey <44633479+HoneyChasey@users.noreply.github.com>
2026-08-02 16:17:15 +0200

    Fix: missing part of the config

--- nvarch-config/.config/nvim/lua/configs/lspconfig.lua
+++ nvarch-config/.config/nvim/lua/configs/lspconfig.lua
@@ -12,3 +12,24 @@ vim.diagnostic.config({
 
 -- read :h vim.lsp.config for changing options of lsp servers 
 --
+-- Hightlighing sames variables. May break after change, so check news on neovim
+local function lsp_highlight_document(client)
+    if client.server_capabilities.documentHighlightProvider then
+        vim.api.nvim_exec([[
+            augroup lsp_document_highlight
+                autocmd! * <buffer>
+                autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+                autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+            augroup END
+            ]],
+        false)
+    end
+end
+
+-- Attach the fuction after the lps on the current buffer load. Thx Claude
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        lsp_highlight_document(client)
+    end,
+})
