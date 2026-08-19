0d380cc607271987b125c3f8587c5ee105883f3e
HoneyChasey <44633479+HoneyChasey@users.noreply.github.com>
2026-08-19 16:55:34 +0200

    Fix: Remove mason from the config
    Signed-off-by: HoneyChasey <44633479+HoneyChasey@users.noreply.github.com>

--- nvarch-config/.config/nvim/lua/configs/mason-lspconfig.lua
+++
@@ -1,9 +0,0 @@
-local servers = require("configs.servers")
-
-return {
-  require("mason-lspconfig").setup({
-    ensure_installed = servers
-  })
-}
-
-
--- nvarch-config/.config/nvim/lua/plugins/mason.lua
+++
@@ -1,5 +0,0 @@
--- Package manager for LSP servers, linters, and formatters
-return {
-    "mason-org/mason.nvim",
-    opts = {},
-}
