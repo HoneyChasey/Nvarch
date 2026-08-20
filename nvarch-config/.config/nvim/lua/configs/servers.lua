70de7226ee07e17682f74a706ea06684a602c7b5
HoneyChasey <44633479+HoneyChasey@users.noreply.github.com>
2026-08-20 10:19:05 +0200

    Hotfix: nil_ls not active on the list of servers
    Signed-off-by: HoneyChasey <44633479+HoneyChasey@users.noreply.github.com>

--- nvarch-config/.config/nvim/lua/configs/servers.lua
+++ nvarch-config/.config/nvim/lua/configs/servers.lua
@@ -1,7 +1,8 @@
 -- This files only contain the array of servers used by mason-lspconfig, mason, and nvim-lspconfig
 
 
-return { "html", "cssls", "bashls", "clangd", "dockerls", "docker_compose_language_service", "gopls", "jdtls", "lua_ls", "pyright", "svelte", "rust_analyzer", "texlab"} -- Write here all the the name of lsp you want to enable. To get their name, please refer to https://github.com/neovim/nvim-lspconfig--
+return { "html", "cssls", "bashls", "clangd", "dockerls", "docker_compose_language_service", "gopls", "jdtls", "lua_ls", "pyright", "svelte", "rust_analyzer", "texlab", "nil_ls"} 
+-- Write here all the the name of lsp you want to enable. To get their name, please refer to https://github.com/neovim/nvim-lspconfig--
 
 
 --Exemple i want to add enable svelte to my nvim go to the repo and search the name of the language.--
